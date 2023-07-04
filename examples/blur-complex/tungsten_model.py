c7c7d1dd749e435d030a22f551fe2fa6d0c31d16
mjpyeon <myeongjang.pyeon@gmail.com>
2023-07-04 23:01:26 +0900

    example update

--- examples/blur-complex/tungsten_model.py
+++ examples/blur-complex/tungsten_model.py
@@ -8,20 +8,28 @@ from tungstenkit import BaseIO, Field, Image, Option, define_model
 
 class Input(BaseIO):
     image: Image = Field(description="Image to blur")
+    text: str = Field(description="hello")
     gaussian_kernel_radius: int = Option(
         5, description="Gaussian kernel size for blurring", ge=1, le=10
     )
-    delay: float = Option(1.0, description="Delay in seconds", ge=0.1, le=60.0)
+    delay: int = Option(1, description="Delay in seconds", ge=1, le=60.0)
+    failure: bool = Option(False)
+    empty: str = Option("")
 
 
 class Output(BaseIO):
-    blurred: Image
+    blurred: List[Image]
+
+
+class DemoOutput(Output):
+    original: Image
+    bluured_dict: Dict[str, Image]
 
 
 @define_model(
     input=Input,
     output=Output,
-    demo_output=Output,
+    demo_output=DemoOutput,
     readme_md="README.md",
     gpu=False,
     python_packages=["pillow"],
@@ -36,25 +44,35 @@ class ModelData:
         converted = []
         for img in images:
             converted.append(img.filter(ImageFilter.GaussianBlur(radius=5)).convert("RGB"))
-        return [Output(blurred=Image.from_pil_image(img)) for img in images]
+        return [Output(blurred=[Image.from_pil_image(img)]) for img in images]
 
     def predict_demo(self, inputs: List[Input]) -> Tuple[List[Output], List[Dict]]:
         opt = inputs[0]
         pil_images = [inp.image.to_pil_image() for inp in inputs]
         for i, pil_img in enumerate(pil_images):
             print(
-                f"[{i+1:02} / {len(pil_images):02}]"
+                f"[{i+1:02} / {len(pil_images):02}] "
                 + f"running image processing (delay: {opt.delay})",
-                end="",
             )
             pil_img.filter(ImageFilter.GaussianBlur(radius=opt.gaussian_kernel_radius)).convert(
                 "RGB"
             ).save(f"image-{i}.jpg")
-            time.sleep(opt.delay)
-            print(" done")
+            for _ in range(opt.delay):
+                for __ in range(2):
+                    print("hello")
+                    time.sleep(0.5)
 
+        if opt.failure:
+            raise RuntimeError("Failure")
         blurred_images = [Image.from_path(path=f"image-{i}.jpg") for i in range(len(pil_images))]
         demo_outputs = [
-            {"original": img, "blurred": blurred} for img, blurred in zip(inputs, blurred_images)
+            {
+                "original": inp.image,
+                "blurred": [blurred, blurred, blurred],
+                "bluured_dict": {"key1": blurred, "key2": blurred},
+            }
+            for inp, blurred in zip(inputs, blurred_images)
         ]
-        return [Output(blurred=blurred) for blurred in blurred_images], demo_outputs
+        return [
+            Output(blurred=[blurred, blurred, blurred]) for blurred in blurred_images
+        ], demo_outputs
