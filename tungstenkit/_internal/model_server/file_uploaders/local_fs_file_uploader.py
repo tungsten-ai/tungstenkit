d3095ba9047c3bd5d9250cb041fd3cba972d39bb
mjpyeon <myeongjang.pyeon@gmail.com>
2023-07-14 01:07:38 +0900

    fix file uploader bug

--- tungstenkit/_internal/model_server/file_uploaders/local_fs_file_uploader.py
+++ tungstenkit/_internal/model_server/file_uploaders/local_fs_file_uploader.py
@@ -1,8 +1,11 @@
+import shutil
+import time
 import typing as t
 from concurrent.futures import Future, ThreadPoolExecutor
 from pathlib import Path
 from uuid import uuid4
 
+from tungstenkit import exceptions
 from tungstenkit._internal import io
 from tungstenkit._internal.utils.uri import get_path_from_file_url
 
@@ -23,14 +26,25 @@ class LocalFSFileUploader(AbstractFileUploader):
         with ThreadPoolExecutor(max_workers=8) as executor:
             for idx, src in executor.map(self._save, range(len(files)), files):
                 dest = self.mount_point / ("output-" + uuid4().hex + "-" + src.name)
-                future_dict[idx] = executor.submit(self._copy, src, dest)
+                future_dict[idx] = executor.submit(shutil.copy, str(src), str(dest))
 
             saved_paths: t.List[Path] = [
-                future_dict[i].result(timeout=60) for i in range(len(files))
+                Path(future_dict[i].result(timeout=60)) for i in range(len(files))
             ]
-        assert all(p.exists() for p in saved_paths), "Failed to save files: " + ", ".join(
-            [f.__root__ for f in files]
-        )
+
+        # Wait for sync with docker volume
+        start_time = time.monotonic()
+        all_files_exist = all(p.exists() for p in saved_paths)
+        while all_files_exist and time.monotonic() - start_time < 10:
+            all_files_exist = all(p.exists() for p in saved_paths)
+            if all_files_exist:
+                break
+            else:
+                time.sleep(0.1)
+
+        if not all_files_exist:
+            raise exceptions.UploadError("Failed to save files to the volume")
+
         out = [type_dict[i].from_path(p) for i, p in enumerate(saved_paths)]
         return out
 
@@ -38,11 +52,3 @@ class LocalFSFileUploader(AbstractFileUploader):
         file_uri = io.URIForFile(file.__root__).to_file_uri()
         path = get_path_from_file_url(file_uri)
         return idx, path
-
-    def _copy(self, src: Path, dest: Path) -> Path:
-        with src.open("rb") as f_read:
-            with dest.open("wb") as f_write:
-                b = f_read.read(BUFFER_SIZE)
-                f_write.write(b)
-
-        return dest
