685015430e9b40bb3d5cf8db48aa82b092bad0ff
mjpyeon <myeongjang.pyeon@gmail.com>
2023-08-22 14:25:22 +0900

    bugfix

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,37 +1,20 @@
 import { Box, LoadingOverlay } from "@mantine/core";
 import Head from "next/head";
 import getModelAPI from "@/api/model";
-import { Cookies } from "react-cookie";
 import ModelPageLayout from "@/components/layouts/ModelPageLayout";
 import { getClientSideAxios } from "@/axios";
 import ModelRun from "@/components/model-run/ModelRun";
-import { Model } from "@/types";
-import { Prediction } from "@/api/prediction.types";
-import getPredictionAPI from "@/api/prediction";
-import { useEffect, useState, useLayoutEffect } from "react";
-import { showFailureNotification } from "@/components/common/notifications";
+import { useQuery, QueryClientProvider, QueryClient } from "react-query";
 
 export default function ModelRunPage() {
+  const queryClient = new QueryClient()
   const axiosInstance = getClientSideAxios();
   const modelAPI = getModelAPI(axiosInstance);
 
-
-  const [model, setModel] = useState<Model | null>(null);
-  
-  useLayoutEffect(() => {
-    async function load_model() {
-      await modelAPI
-        .get()
-        .then((response) => setModel(response.data))
-        .catch(() => {
-          showFailureNotification("Failed to load model metadata by unexpected server error")
-        });
-    }
-    load_model()
-  }, [])
-
+  const { data: model } = useQuery(["model"], () => modelAPI.get().then((resp) => resp.data))
 
   return (
+    <QueryClientProvider client={queryClient}>
     <Box>
       {model ? (
         <>
@@ -48,5 +31,6 @@ export default function ModelRunPage() {
       ) : undefined}
       <LoadingOverlay visible={model == null}/>
     </Box>
+    </QueryClientProvider>
   );
 }
